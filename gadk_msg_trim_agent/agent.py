54e3d6c9bd60ea41356f60747bf5421464563b05
dumidu.rajapaksha <91795441+Dumidu1212@users.noreply.github.com>
2025-07-11 22:40:56 +0530

    Add deploy folder files

--- gadk_msg_trim_agent/agent.py
+++ gadk_msg_trim_agent/agent.py
@@ -1,12 +1,12 @@
-from google.akd.agents import Agent
+from google.adk.agents import Agent
 
 from gadk_msg_trim_agent.prompt import ROOT_AGENT_INSTRUCTION
 from gadk_msg_trim_agent.tools import count_characters
 
 root_agent = Agent(
-  name="gadk_msg_trim_agent",
-  model="gemini-2.0-flash",
-  description="A bot that shortens messages while maintaining their core meaning",
-  instruction=ROOT_AGENT_INSTRUCTION,
-  tools=[count_characters],
+    name="adk_short_bot",
+    model="gemini-2.0-flash",
+    description="A bot that shortens messages while maintaining their core meaning",
+    instruction=ROOT_AGENT_INSTRUCTION,
+    tools=[count_characters],
 )
