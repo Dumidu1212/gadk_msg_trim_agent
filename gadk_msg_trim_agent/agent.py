889ae00f7a091e7ef8fce0d075e8942fb4984714
dumidu.rajapaksha <91795441+Dumidu1212@users.noreply.github.com>
2025-07-11 22:48:39 +0530

    Update agent.py

--- gadk_msg_trim_agent/agent.py
+++ gadk_msg_trim_agent/agent.py
@@ -4,7 +4,7 @@ from gadk_msg_trim_agent.prompt import ROOT_AGENT_INSTRUCTION
 from gadk_msg_trim_agent.tools import count_characters
 
 root_agent = Agent(
-    name="adk_short_bot",
+    name="gadk_msg_trim_agent",
     model="gemini-2.0-flash",
     description="A bot that shortens messages while maintaining their core meaning",
     instruction=ROOT_AGENT_INSTRUCTION,
